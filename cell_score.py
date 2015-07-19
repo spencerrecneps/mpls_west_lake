cdfd13ea4d70036a7a53b75555075274c4d7cd0b
Spencer Gardner <spencergardner@gmail.com>
2015-07-18 20:30:35 -0500

    working network

--- cell_score.py
+++ cell_score.py
@@ -1,3 +1,5 @@
+#Definition of inputs and outputs
+#==================================
 ##Toole=group
 ##Connectivity score=name
 ##Grid_layer=vector
@@ -9,11 +11,15 @@
 ##Road_layer=vector
 ##Output_paths=output vector
 
+#Algorithm body
+#==================================
 from qgis.core import *
+from PyQt4.QtCore import *
+from processing.tools.vector import VectorWriter
 import networkx as nx
 
 #get QGIS objects
-grid = processing.getObjectFromUri(Grid_layer)
+grid = processing.getObject(Grid_layer)
 network = processing.getObjectFromUri(Network_table)
 roads = processing.getObjectFromUri(Road_layer)
 
@@ -41,3 +47,24 @@ SG = nx.DiGraph( [ (u,v,d) for u,v,d in DG.edges(data=True) if d['stress'] <= Ma
 progress.setText('Getting shortest path with stress')
 pathStress = nx.shortest_path(SG,source=From_ID,target=To_ID,weight='weight')
 progress.setInfo('Shortest constrained path: ' + str(pathStress))
+
+#write shortest paths to new vector layer
+provider = roads.dataProvider()
+writer = VectorWriter(Output_paths, None, [QgsField("road_id", QVariant.Int),QgsField("cost", QVariant.Int),QgsField("path_type", QVariant.String)], provider.geometryType(), provider.crs() )
+for feat in processing.features(roads):
+    if feat.attribute('id') in pathNoStress:
+        newFeat = QgsFeature()
+        newFeat.setGeometry(feat.geometry())
+        newFeat.initAttributes(3)
+        newFeat.setAttribute(0,feat.attribute('id'))
+        newFeat.setAttribute(1,feat.attribute('cost'))
+        newFeat.setAttribute(2,'Unconstrained')
+        writer.addFeature( newFeat )
+    if feat.attribute('id') in pathStress:
+        newFeat = QgsFeature()
+        newFeat.setGeometry(feat.geometry())
+        newFeat.initAttributes(3)
+        newFeat.setAttribute(0,feat.attribute('id'))
+        newFeat.setAttribute(1,feat.attribute('cost'))
+        newFeat.setAttribute(2,'Constrained')
+        writer.addFeature( newFeat )
